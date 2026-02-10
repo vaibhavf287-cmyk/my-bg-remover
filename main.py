aea08b3c757bcb7131bcdc88528b1391d42cd65e
vaibhavf287-cmyk <vaibhavf287@gmail.com>
2026-02-10 12:53:26 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 from flask import Flask, request, send_file
 from rembg import remove
 import io
+import os
 
 app = Flask(__name__)
+
+# Memory bachane ke liye ONNX settings
+os.environ["OMP_NUM_THREADS"] = "1"
 
 @app.route('/')
 def home():
@@ -10,17 +14,24 @@ def home():
 
 @app.route('/remove-bg', methods=['POST'])
 def remove_bg():
-    # Hum check kar rahe hain ki 'image_file' naam ka data aaya ya nahi
     if 'image_file' not in request.files:
-        return "No image_file found in request", 400
+        return "No image file", 400
     
     try:
         file = request.files['image_file']
-        input_image = file.read()
-        output_image = remove(input_image)
-        return send_file(io.BytesIO(output_image), mimetype='image/png')
+        input_data = file.read()
+        
+        # Image process karte waqt memory kam use ho isliye direct return
+        output_data = remove(input_data)
+        
+        return send_file(
+            io.BytesIO(output_data),
+            mimetype='image/png'
+        )
     except Exception as e:
         return str(e), 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=10000)
+    port = int(os.environ.get('PORT', 10000))
+    app.run(host='0.0.0.0', port=port)
+0
